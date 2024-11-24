ab66958a476960ee600d9e6c2f21ac20431d900c
Xinye Wang <xinyew@gatech.edu>
2024-11-24 17:15:01 -0500

    buffer stuffing functionality tested with Arduino

--- float_array_print/float_array_print.ino
+++ float_array_print/float_array_print.ino
@@ -1,27 +1,45 @@
+int indices[5] = {-1, 0, 1, 2, 3};
+double data[5] = {
+    4.0,
+    0.0000555273,
+    0.0002756368,
+    0.0000666020,
+    0.0002730220,};
+int length;
+
+uint8_t buffer[4096];
+
+void stuff_buffer() {
+    int len = (sizeof(indices)/sizeof(indices[0]));
+    length = len * 12;
+
+    uint8_t *tmp_d = (uint8_t*) data;
+    uint8_t *tmp_i = (uint8_t*) indices;
+
+    for (int i = 0; i < len; i++) {
+        for (int j = 0; j < 4; j++) {
+            buffer[12*i + j] = tmp_i[4*i + j];
+        }
+        for (int j = 0; j < 8; j++) {
+            buffer[12*i + j + 4] = tmp_d[8*i + j];
+        }
+    }
+}
+
+
 void setup() {
   Serial.begin(9600);
-
+  stuff_buffer();
   delay(100);
-  double myDouble = 5.678;
-  int myInt = doubleToInt(myDouble);
+  Serial.println("Begin");
+  for (int i = 0; i < length/12; i++) {
+    int tt = *((int*)(buffer + 12 * i));
+    double ttt = *((double*)(buffer + 12 * i + 4));
+    Serial.printf("%3d: %.10f\n", tt, ttt);
+  }
 
-  Serial.print("Double to Int: ");
-  Serial.println(myInt); // Output: 5
-
-  int anotherInt = 42;
-  double anotherDouble = intToDouble(anotherInt);
-  Serial.print("Int to Double: ");
-  Serial.println(anotherDouble, 3); // Output: 42.000
 }
 
 void loop() {
   // Do nothing
-}
-
-int doubleToInt(double value) {
-  return (int)value; // Convert double to int by truncation
-}
-
-double intToDouble(int value) {
-  return (double)value; // Convert int to double
 }
